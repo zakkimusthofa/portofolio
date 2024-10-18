67d39c4ce8ea4e69b87c8d2662b5886f7f6fe3d5
zakkimusthofa <zakkimusthofa4@gmail.com>
2024-10-18 22:42:16 +0700

    push last update

--- script/script.js
+++ script/script.js
@@ -8,15 +8,37 @@ const item2 = document.querySelector('.nexttt')
 
 buttom.addEventListener('click', (e) => {
     item.classList.toggle('active');
+    // buttom.classList.toggle('active');
+    // buttom.textContent("minimize")
+    if (item.classList.contains("active")) {
+        buttom.textContent = "minimize"
+    } else {
+        buttom.textContent = "read More..."
+    }
     console.log('ok')
 });
 
 buttom1.addEventListener('click', (e) => {
     item1.classList.toggle('active'); // Menggunakan item1
+    // buttom1.classList.toggle('active'); // Menggunakan item1
+    // buttom1.textContent("minimize")
+    if (item1.classList.contains("active")) {
+        buttom1.textContent = "minimize"
+    } else {
+        buttom1.textContent = "read More..."
+    }
     console.log('ok1')
 })
 
 buttom2.addEventListener('click', (e) => {
     item2.classList.toggle('active'); // Menggunakan item2
+    // buttom2.classList.toggle('active'); // Menggunakan item2
+    // buttom2.textContent("minimize")
+    if (item2.classList.contains("active")) {
+        buttom2.textContent = "minimize"
+    } else {
+        buttom2.textContent = "read More..."
+    }
+
     console.log('ok2')
 });
